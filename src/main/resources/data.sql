71669f1e426f5e6d8c1676d67b74087c0c6f99f3
srikanth <srikanth.parvatikar@traveloka.com>
2020-04-26 15:20:45 +0530

    springBoot basic Architecture

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values ('101', 'Srikanth', 'Java' );
+insert into alien values ('102', 'Ravi', 'Android' );
+insert into alien values ('103', 'Pramod', 'PHP' );
+insert into alien values ('104', 'Laxmikanth', 'GO' );
+insert into alien values ('105', 'Ankur', 'JavaScript' );
